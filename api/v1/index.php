fbc3a41b1098870453276198c5b5428a1e53c31a
Matt Thomas <matt@betweenbrain.com>
2014-09-23 13:25:34 -0400

    PHPCS

--- api/v1/index.php
+++ api/v1/index.php
@@ -24,41 +24,48 @@ $app->_db = JFactory::getDbo();
 $app->view(new \JsonApiView());
 $app->add(new \JsonApiMiddleware());
 
-//Main entry
+// Main entry
 $app->get('/', function () use ($app)
-{
-	$app->render(200, array(
-		'msg' => 'You reach the JAB API V1',
-	));
-});
+	{
+		$app->render(
+			200, array(
+				'msg' => 'You reach the JAB API V1',
+			)
+		);
+	}
+);
 
 // Content
 $app->map('/content/', function () use ($app)
-{
-	$query = $app->_db->getQuery(true);
-	$query->select('*')
-		->from($app->_db->quoteName('#__content'))
-		->where($app->_db->quoteName('state') . ' = ' . $app->_db->quote('1'));
-	$app->_db->setQuery($query);
+	{
+		$query = $app->_db->getQuery(true);
+		$query->select('*')
+			->from($app->_db->quoteName('#__content'))
+			->where($app->_db->quoteName('state') . ' = ' . $app->_db->quote('1'));
+		$app->_db->setQuery($query);
 
-	$app->render(200, array(
-		'msg' => $app->_db->loadObjectList(),
-	));
-})->via('GET');
+		$app->render(200, array(
+				'msg' => $app->_db->loadObjectList(),
+			)
+		);
+	}
+)->via('GET');
 
 $app->map('/content/:id', function ($id) use ($app)
-{
-	$query = $app->_db->getQuery(true);
-	$query->select('*')
-		->from($app->_db->quoteName('#__content'))
-		->where('id = ' . $app->_db->quote($id)
-			. ' AND ' . $app->_db->quoteName('state') . ' = ' . $app->_db->quote('1')
-		);
-	$app->_db->setQuery($query);
+	{
+		$query = $app->_db->getQuery(true);
+		$query->select('*')
+			->from($app->_db->quoteName('#__content'))
+			->where('id = ' . $app->_db->quote($id)
+				. ' AND ' . $app->_db->quoteName('state') . ' = ' . $app->_db->quote('1')
+			);
+		$app->_db->setQuery($query);
 
-	$app->render(200, array(
-		'msg' => $app->_db->loadObject(),
-	));
-})->via('GET');
+		$app->render(200, array(
+				'msg' => $app->_db->loadObject(),
+			)
+		);
+	}
+)->via('GET');
 
 $app->run();
